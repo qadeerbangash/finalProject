d42b2ec7d893058e4f10460e7c9f09cd7265fe5f
Qadeer Hussain <qadeeerhussain494@gmail.com>
2022-10-15 12:38:14 +0500

    Complete Work

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+	class cleanto_myvariable{
+	public $hostnames = "localhost";
+	public $username = "root";
+	public $passwords = "";
+	public $database = "finalproject";
+	public $epcode = "rfgdssad5435";
+} ?>
